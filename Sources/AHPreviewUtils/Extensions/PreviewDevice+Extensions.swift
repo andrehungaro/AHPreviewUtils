7629bb5333e206becaf8827897ae349dfaf429ed
Andre Hungaro <hungaro.andre@gmail.com>
2020-07-08 21:09:56 -0700

    Adding PreviewDevice extension

---
+++ Sources/AHPreviewUtils/Extensions/PreviewDevice+Extensions.swift
@@ -0,0 +1,66 @@
+//
+//  PreviewDevice+Extensions.swift
+//  
+//
+//  Created by Andre Hungaro on 2020-07-08.
+//
+
+import Foundation
+
+public extension PreviewDevice {
+    
+    static let iPhone_4s = PreviewDevice(rawValue: "iPhone 4s")
+    static let iPhone_5 = PreviewDevice(rawValue: "iPhone 5")
+    static let iPhone_5s = PreviewDevice(rawValue: "iPhone 5s")
+    static let iPhone_6_Plus = PreviewDevice(rawValue: "iPhone 6 Plus")
+    static let iPhone_6 = PreviewDevice(rawValue: "iPhone 6")
+    static let iPhone_6s = PreviewDevice(rawValue: "iPhone 6s")
+    static let iPhone_6s_Plus = PreviewDevice(rawValue: "iPhone 6s Plus")
+    static let iPhone_SE_1st_generation = PreviewDevice(rawValue: "iPhone SE (1st generation)")
+    static let iPhone_7 = PreviewDevice(rawValue: "iPhone 7")
+    static let iPhone_7_Plus = PreviewDevice(rawValue: "iPhone 7 Plus")
+    static let iPhone_8 = PreviewDevice(rawValue: "iPhone 8")
+    static let iPhone_8_Plus = PreviewDevice(rawValue: "iPhone 8 Plus")
+    static let iPhone_X = PreviewDevice(rawValue: "iPhone X")
+    static let iPhone_Xs = PreviewDevice(rawValue: "iPhone Xs")
+    static let iPhone_Xs_Max = PreviewDevice(rawValue: "iPhone Xs Max")
+    static let iPhone_Xʀ = PreviewDevice(rawValue: "iPhone Xʀ")
+    static let iPhone_11 = PreviewDevice(rawValue: "iPhone 11")
+    static let iPhone_11_Pro = PreviewDevice(rawValue: "iPhone 11 Pro")
+    static let iPhone_11_Pro_Max = PreviewDevice(rawValue: "iPhone 11 Pro Max")
+    static let iPhone_SE_2nd_generation = PreviewDevice(rawValue: "iPhone SE (2nd generation)")
+    static let iPad_2 = PreviewDevice(rawValue: "iPad 2")
+    static let iPad_Retina = PreviewDevice(rawValue: "iPad Retina")
+    static let iPad_Air = PreviewDevice(rawValue: "iPad Air")
+    static let iPad_mini_2 = PreviewDevice(rawValue: "iPad mini 2")
+    static let iPad_mini_3 = PreviewDevice(rawValue: "iPad mini 3")
+    static let iPad_mini_4 = PreviewDevice(rawValue: "iPad mini 4")
+    static let iPad_Air_2 = PreviewDevice(rawValue: "iPad Air 2")
+    static let iPad_Pro_9_7inch = PreviewDevice(rawValue: "iPad Pro (9.7-inch)")
+    static let iPad_Pro_12_9inch = PreviewDevice(rawValue: "iPad Pro (12.9-inch)")
+    static let iPad_5th_generation = PreviewDevice(rawValue: "iPad (5th generation)")
+    static let iPad_Pro_12_9inch_2nd_generation = PreviewDevice(rawValue: "iPad Pro (12.9-inch) (2nd generation)")
+    static let iPad_Pro_10_5inch = PreviewDevice(rawValue: "iPad Pro (10.5-inch)")
+    static let iPad_6th_generation = PreviewDevice(rawValue: "iPad (6th generation)")
+    static let iPad_7th_generation = PreviewDevice(rawValue: "iPad (7th generation)")
+    static let iPad_Pro_11inch_1st_generation = PreviewDevice(rawValue: "iPad Pro (11-inch) (1st generation)")
+    static let iPad_Pro_12_9inch_3rd_generation = PreviewDevice(rawValue: "iPad Pro (12.9-inch) (3rd generation)")
+    static let iPad_Pro_11inch_2nd_generation = PreviewDevice(rawValue: "iPad Pro (11-inch) (2nd generation)")
+    static let iPad_Pro_12_9inch_4th_generation = PreviewDevice(rawValue: "iPad Pro (12.9-inch) (4th generation)")
+    static let iPad_mini_5th_generation = PreviewDevice(rawValue: "iPad mini (5th generation)")
+    static let iPad_Air_3rd_generation = PreviewDevice(rawValue: "iPad Air (3rd generation)")
+    static let tv = PreviewDevice(rawValue: "Apple TV")
+    static let tv_4K = PreviewDevice(rawValue: "Apple TV 4K")
+    static let tv_4K_at_1080p = PreviewDevice(rawValue: "Apple TV 4K (at 1080p)")
+    static let watch_38mm = PreviewDevice(rawValue: "Apple Watch - 38mm")
+    static let watch_42mm = PreviewDevice(rawValue: "Apple Watch - 42mm")
+    static let watch_Series_2_38mm = PreviewDevice(rawValue: "Apple Watch Series 2 - 38mm")
+    static let watch_Series_2_42mm = PreviewDevice(rawValue: "Apple Watch Series 2 - 42mm")
+    static let watch_Series_3_38mm = PreviewDevice(rawValue: "Apple Watch Series 3 - 38mm")
+    static let watch_Series_3_42mm = PreviewDevice(rawValue: "Apple Watch Series 3 - 42mm")
+    static let watch_Series_4_40mm = PreviewDevice(rawValue: "Apple Watch Series 4 - 40mm")
+    static let watch_Series_4_44mm = PreviewDevice(rawValue: "Apple Watch Series 4 - 44mm")
+    static let watch_Series_5_40mm = PreviewDevice(rawValue: "Apple Watch Series 5 - 40mm")
+    static let watch_Series_5_44mm = PreviewDevice(rawValue: "Apple Watch Series 5 - 44mm")
+
+}
