5213c8ffedfa1627260f22ee827df90fe7e9bfcf
Andre Hungaro <hungaro.andre@gmail.com>
2020-08-05 20:53:30 -0700

    Fixing access to PreviewGroup

--- Sources/AHPreviewUtils/PreviewGroup.swift
+++ Sources/AHPreviewUtils/PreviewGroup.swift
@@ -7,19 +7,19 @@
 
 import SwiftUI
 
-struct PreviewGroup<Content: View>: View  {
+public struct PreviewGroup<Content: View>: View  {
     
-    typealias ContentView = () -> Content
+    public typealias ContentView = () -> Content
     
     private let content: ContentView
     private let devices: [PreviewDevice]
     
-    init(devices: [PreviewDevice], @ViewBuilder _ content: @escaping ContentView) {
+    public init(devices: [PreviewDevice], @ViewBuilder _ content: @escaping ContentView) {
         self.content = content
         self.devices = devices
     }
     
-    var body: some View {
+    public var body: some View {
         Group {
             ForEach(0..<devices.count) { index in
                 content()
